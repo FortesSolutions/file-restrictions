e830af8a8184ba14d0addef0b76758c73a2f2a78
fabian <f.kruders@fortes.nl>
2021-07-14 10:53:03 +0200

    Added isBlockedFileName and isBlockedFileType

--- index.js
+++ index.js
@@ -3,7 +3,42 @@ import BLOCKED_FILE_TYPES from "./blockedExtensions.json";
 
 const SIZE_LIMIT = 250 * 1000 * 1000; // 250 MB
 
+function isBlockedFileName(fileName) {
+	if (typeof fileName !== 'string') {
+		throw new Error('Expected a string');
+	}
+
+	// Blocked characters
+	const forbidden = /[\\/:*?"<>|]/g;
+	const leading = /^[ ]+/g;
+	const trailing = /[. ]+$/g;
+
+	return (forbidden.test(fileName) || leading.test(fileName) || trailing.test(fileName));
+}
+
+/**
+ * Checks whether file name has a match in BLOCKED_FILE_TYPES
+ * @param {string} fileName
+ * @returns {boolean}
+ */
+function isBlockedFileType(fileName) {
+	if (typeof fileName !== 'string') {
+		throw new Error('Expected a string');
+	}
+
+	const matches = fileName.match(new RegExp(/\.[^.]+$/)); // Match last dot and any following characters
+
+	if (!matches) {
+		return false;
+	}
+
+	const fileType = matches[0].toLocaleLowerCase();
+	return BLOCKED_FILE_TYPES.includes(fileType);
+}
+
 export {
 	SIZE_LIMIT,
-	BLOCKED_FILE_TYPES
+	BLOCKED_FILE_TYPES,
+	isBlockedFileName,
+	isBlockedFileType
 }
